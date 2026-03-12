f75e11f425f41b674c2e45a284b09e48a798f278
chishige1217200 <daichicchi01@gmail.com>
2026-03-12 23:01:10 +0900

    Add: 音楽コードを管理するpyファイルを作成

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import time
 import os
 from dotenv import load_dotenv
 
+import note
+
 # .envファイルの内容を読み込む
 load_dotenv()
 SF2_PATH = os.environ.get("SF2_PATH")
@@ -10,16 +12,21 @@ SF2_PATH = os.environ.get("SF2_PATH")
 if SF2_PATH is None:
     raise ValueError("SF2_PATHが読み込めません。.envファイルが存在するか確認してください。")
 
+# FluidSynthの初期化とSoundfontのロード
 fs = fluidsynth.Synth()
 fs.start()
 
 sfid = fs.sfload(SF2_PATH)
 fs.program_select(0, sfid, 0, 0)
 
-fs.noteon(0, 60, 120)
-fs.noteon(0, 64, 120)
-fs.noteon(0, 67, 120)
+# 和音のMIDIノート番号を取得して、和音を鳴らす
+notes = note.major_chord(note.note_to_midi("C", 4))
+for n in notes:
+    fs.noteon(0, n, 120)
 
 time.sleep(3)
 
-fs.noteoff(0, 60)
+for n in notes:
+    fs.noteoff(0, n)
+
+fs.delete()
